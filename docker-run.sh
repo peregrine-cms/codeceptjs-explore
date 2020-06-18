b8e6e0b4e19544b298616af9ae4a3d552670d29e
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-06-18 10:10:01 -0700

    Solve local and docker run conflict issue and improved performance.

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Since we are mounting our workspace and sharing it with Docker, we need to
-# make sure that we remove the node_modules to ensure there are no Node conflicts
-# between tests running on the local host and those running on the Docker container.
-if [ -e `pwd`/node_modules ]; then
-  rm -rf `pwd`/node_modules
+if [ ! -e node_modules.docker ]; then
+  mkdir node_modules.docker
 fi
+
+if [ -e node_modules ]; then
+  rm node_modules
+fi
+
+ln -s node_modules.docker node_modules
 
 docker run --net=host \
 	-v $PWD:/tests \
         -w /tests \
 	peregrinecms/e2e-tests:latest \
-        sh -c 'npm i --save-dev && mkdir -p output; xvfb-run -s ":99 -auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24" ffmpeg -y -f x11grab -video_size 1920x1080 -i :99 -codec:v libx264 -pix_fmt yuv420p  -draw_mouse 0 -r 12 -loglevel panic output/video.mp4& DISPLAY=:99  npx codeceptjs run --steps; killall ffmpeg; sleep 5'
+        sh -c 'npm i && mkdir -p output; xvfb-run -s ":99 -auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24" ffmpeg -y -f x11grab -video_size 1920x1080 -i :99 -codec:v libx264 -pix_fmt yuv420p  -draw_mouse 0 -r 12 -loglevel panic output/video.mp4& DISPLAY=:99  npx codeceptjs run --steps; killall ffmpeg; sleep 5'
