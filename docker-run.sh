1b30547663c26b12e3f8ffef395cd29adab4ae06
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-06-18 10:29:06 -0700

    Added timestamp to video output file.

--- docker-run.sh
+++ docker-run.sh
@@ -14,4 +14,4 @@ docker run --net=host \
 	-v $PWD:/tests \
         -w /tests \
 	peregrinecms/e2e-tests:latest \
-        sh -c 'npm i && mkdir -p output; xvfb-run -s ":99 -auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24" ffmpeg -y -f x11grab -video_size 1920x1080 -i :99 -codec:v libx264 -pix_fmt yuv420p  -draw_mouse 0 -r 12 -loglevel panic output/video.mp4& DISPLAY=:99  npx codeceptjs run --steps; killall ffmpeg; sleep 5'
+        sh -c 'npm i && mkdir -p output; xvfb-run -s ":99 -auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24" ffmpeg -y -f x11grab -video_size 1920x1080 -i :99 -codec:v libx264 -pix_fmt yuv420p  -draw_mouse 0 -r 12 -loglevel panic output/video-`date +%Y-%m-%d-%H%M%S`.mp4& DISPLAY=:99  npx codeceptjs run --steps; killall ffmpeg; sleep 5'
