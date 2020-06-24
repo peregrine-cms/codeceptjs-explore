33011d3b521881600943df95d436ef611d46ecf6
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-06-24 08:40:41 -0700

    Added wrappers for native xdotool mouse events

--- dnd_test.js
+++ dnd_test.js
@@ -2,21 +2,22 @@ Feature('drag-and-drop');
 
 Scenario('Navigate to site', async (I) => {
 
-  I.getMouseLocation()
-
   // HTM5 Drag and Drop does not work. Unless OS mouse is manually placed over drop zone.
   I.amOnPage('https://bestvpn.org/html5demos/drag/')
   I.showCursor()
-  I.dragAndDropNative('#one', '#bin')
+  I.nativeDragAndDrop('#one', '#bin')
+  I.nativeDragAndDrop('#two', '#bin')
+  I.nativeDragAndDrop('#three', '#bin')
+  I.nativeDragAndDrop('#four', '#bin')
   //pause()
 
   // This works. It's from CodeceptJS' test suite
-  I.amOnPage('https://jqueryui.com/resources/demos/droppable/default.html')
-  I.dragAndDropNative('#draggable', '#droppable')
+  //I.amOnPage('https://jqueryui.com/resources/demos/droppable/default.html')
+  //I.dragAndDropNative('#draggable', '#droppable')
 
   // HTM5 Drag and Drop does not work. Unless OS mouse is manually placed over drop zone.
-  I.amOnPage('https://www.w3schools.com/html/html5_draganddrop.asp')
-  I.dragAndDropNative('#drag1', '#div2')
+  //I.amOnPage('https://www.w3schools.com/html/html5_draganddrop.asp')
+  //I.dragAndDropNative('#drag1', '#div2')
   //pause()
 })
 
--- peregrinemouse_helper.js
+++ peregrinemouse_helper.js
@@ -1,6 +1,6 @@
-const {exec} = require("child_process");
+const childProcess = require("child_process");
 const {Helper} = codeceptjs;
-const windowOffsetY = 120 // height to chrome browser's "top chrome"
+const windowOffsetY = 120 // height of chrome browser's "top chrome"
 
 class PeregrineMouse extends Helper {
 
@@ -8,19 +8,24 @@ class PeregrineMouse extends Helper {
        this.runCmd('xdotool getactivewindow')
     }
 
-    async getMouseLocation() {
-        this.runCmd('xdotool getmouselocation --shell')
-    }
-
-    async movePointer(x, y) {
+    async moveMouse(x, y) {
         this.runCmd(`xdotool mousemove --sync ${x} ${y}`)
         this.runCmd('xdotool getmouselocation --shell')
     }
 
+    async mouseDown() {
+        //this.runCmd(`xdotool mousedown --clearmodifiers 1`)
+        this.runCmd(`xdotool mousedown --clearmodifiers 1 && sleep 1`)
+    }
+
+    async mouseUp() {
+        //this.runCmd(`xdotool mouseup --clearmodifiers 1`)
+        this.runCmd(`xdotool mouseup --clearmodifiers 1 && sleep 1`)
+    }
+
     async runCmd(cmd) {
-        //if (process.env.NO_VIDEO_CC == undefined) {
         if (process.env.NO_VIDEO_CC == 1) {
-            exec(cmd, (error, stdout, stderr) => {
+            childProcess.execSync(cmd, (error, stdout, stderr) => {
                 if (error) {
                     console.log(`error: ${error.message}`);
                     return;
@@ -34,7 +39,7 @@ class PeregrineMouse extends Helper {
         }
     }
 
-    async dragAndDropNative(srcSel, dstSel) {
+    async nativeDragAndDrop(srcSel, dstSel) {
         const {page} = this.helpers.Puppeteer
 
         const source = await page.$(srcSel)
@@ -42,17 +47,23 @@ class PeregrineMouse extends Helper {
 
         const srcX = sourceBox.x + sourceBox.width / 2
         const srcY = sourceBox.y + sourceBox.height / 2
+
         await page.mouse.move(srcX, srcY)
-        this.movePointer(srcX, srcY + windowOffsetY)
+        await this.moveMouse(srcX, srcY + windowOffsetY)
+
         await page.mouse.down(srcX, srcY)
+        await this.mouseDown()
 
         const target = await page.$(dstSel)
         const targetBox = await target.boundingBox()
         const dstX = targetBox.x + (targetBox.width / 2)
         const dstY = targetBox.y + (targetBox.height / 2)
+
         await page.mouse.move(dstX, dstY)
-        this.movePointer(dstX, dstY + windowOffsetY)
+        await this.moveMouse(dstX, dstY + windowOffsetY)
+
         await page.mouse.up()
+        await this.mouseUp()
     }
 }
 
